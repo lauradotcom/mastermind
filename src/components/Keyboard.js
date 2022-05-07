3ce0012c3030ae7fee3651621c1830606370ac43
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2022-05-06 19:53:52 -0700

    Build number keyboard layout

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,5 +1,48 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const KeyboardSection = styled.section`
+  height: 200px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 0.5rem;
+  @media screen and (min-width: 1024px) {
+    width: 40%;
+    margin: 0 auto;
+  }
+`
+
+const KeyboardRow = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`
+
+const KeyboardButton = styled.button`
+  display: flex;
+  justify-items: center;
+  height: 50px;
+  background-color: #8D9F9B;
+  color: #F8F8F8;
+  border-radius: 10px;
+  font-family: 'Inter', sans-serif;
+  padding: 0.5rem;
+  @media screen and (min-width: 1024px) {
+    margin: 0.5rem;
+    padding: 1rem;
+  }
+`
 
 export const Keyboard = () => {
-  return <div><p>Keyboard</p></div>
+  return (
+    <KeyboardSection>
+      <KeyboardRow>
+        {['Enter', 0, 1, 2, 3, 4, 5, 6, 7, 'Back'].map((digit) => {
+          return <KeyboardButton key={digit}>{digit}</KeyboardButton>
+        })}
+      </KeyboardRow>
+    </KeyboardSection>
+  )
 }
