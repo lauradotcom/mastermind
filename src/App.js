d1f8f97d0e56ce5a79f773bd7ff9f11adcdfe952
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2022-05-07 17:54:30 -0700

    Remove unused initBoard component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { useState, useEffect, useRef } from 'react';
 import { Header } from './components/Header';
 import { Gameboard } from './components/Gameboard';
 import { Keyboard } from './components/Keyboard';
-//import { initBoard } from './components/Numbers';
 import { theme } from './styles.js';
 import './App.css';
 
