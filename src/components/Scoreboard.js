3a001ca9532ca0f2d98bb62e9c89e3d338be5c5c
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2022-05-08 00:45:12 -0700

    Rougly lay out feedback columns

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -5,28 +5,53 @@ import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import CheckCircleOutlinedIcon from '@mui/icons-material/CheckCircleOutlined';
 import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
 
+/** Styled Components */
+/** Note: 
+ * "Bulls" = right number, right position
+ * "Cows" = right number, wrong position 
+ * Using because it's a succinct way to name components but come up with something better...
+ */
 
+const FeedbackContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+`
 
+const FeedbackColumn = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 0.25rem 1rem;
+`
 
 export const Scoreboard = () => {
 
   return (
-    <div> 
-      <CheckCircleIcon 
-        sx={{
-          color: '#37A862'
-        }}
-      />
-      <CheckCircleOutlinedIcon
-        sx={{
-          color: '#37A862'
-        }}
-      />
-      <CancelOutlinedIcon
-        sx={{
-          color: '#FD817D'
-        }}
-      />
-    </div>
+    <FeedbackContainer>
+      <FeedbackColumn>
+        <CheckCircleIcon 
+          sx={{
+            color: '#37A862'
+          }}
+        />
+        <CheckCircleOutlinedIcon
+          sx={{
+            color: '#37A862'
+          }}
+        />
+      </FeedbackColumn>
+      <FeedbackColumn>
+        <CheckCircleIcon 
+          sx={{
+            color: '#37A862'
+          }}
+        />
+        <CancelOutlinedIcon
+          sx={{
+            color: '#FD817D'
+          }}
+        />
+      </FeedbackColumn>
+    </FeedbackContainer>
   )
 }
