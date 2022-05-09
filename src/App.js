4b0d93cb612365920aca666962693cd00e5e7aa3
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2022-05-08 22:23:18 -0700

    Initialize answer to empty array and call getAnswer inside Effect hook

--- src/App.js
+++ src/App.js
@@ -11,25 +11,27 @@ import './App.css';
 function App() {
 
   const getAnswer = async () => {
-
     try {
       const response = await axios.get(`https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new`);
       if (response.status === 200) {
         const data = response.data.replace(/\s+/g, ''); //Remove spaces and line breaks
         console.log(data.split(''));
-        return data.split('');
+        setAnswer(data.split(''));
       }
     } 
     catch (error) {
       console.error(error);
       console.error('No dice bro');
-      return ['6', '4', '2', '1'];
+      setAnswer(['6', '4', '2', '1']);
     } 
   }
 
   /** Answer from Integer Generator API */
-  const [answer, setAnswer] = useState(getAnswer);
+  const [answer, setAnswer] = useState([]);
 
+  useEffect(() => {
+    getAnswer();
+  }, []);
 
   /** Initialize the gameboard contents */
   const [guesses, setGuesses] = useState({
