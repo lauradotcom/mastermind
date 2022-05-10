543d916966afeae6d12a756a387c757f45b2c89a
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2022-05-09 21:47:38 -0700

    Lay out Outcome dialog as a modal with opacity backdrop

--- src/components/Outcome.js
+++ src/components/Outcome.js
@@ -2,20 +2,43 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 import { Button } from '../styles';
+import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
 
-const MessageContainer = styled.div`
+const MessageModal = styled.div`
   display: flex;
   position: absolute;
   justify-content: center;
   align-items: center;
-  top: 10vh;
+  height: 100vh;
   width: 100vw;
+  background-color: rgba(0, 0, 0, 0.8);
+  top: 0;
+`
+
+const MessageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  justify-content: space-between;
+  align-items: center;
+  height: 65%;
+  width: 90%;
   background: #111;
   margin: 0 auto;
   border-radius: 10px;
   padding: 1rem;
-  color: #FFF;
+  color: #F9F8F8;
   z-index: 1;
+  @media screen and (min-width: 758px) {
+    height: 45%;
+    width: 50%;
+  }
+  @media screen and (min-width: 1024px) {
+    width: 30%
+  }
+  @media screen and (min-width: 1440px) {
+    width: 25%;
+  }
 `
 const Message = styled.p`
   display: block;
@@ -35,19 +58,25 @@ export const Outcome = ({ outcome, answer }) => {
 
   return (  
     outcome ? (
-      <MessageContainer>
-        {
-        outcome === 'win'? 
-          <Message>You win!
-            <Button>Play Again</Button>
-          </Message> 
-        : <Message>
-            Better luck next time!
-            <Answer>Correct answer: {answer}</Answer>
-            <Button>Try Again</Button>
-          </Message>
-        }
-      </MessageContainer>
+      <MessageModal>
+        <MessageContainer>
+        <CancelOutlinedIcon
+          css={{
+            alignSelf: 'flex-end',
+            color: '#F9F9F8',
+          }}
+        />
+          {
+          outcome === 'win'? 
+            <Message>You win!</Message> 
+          : <Message>
+              Better luck next time!
+              <Answer>Correct answer: {answer}</Answer>
+            </Message>
+          }
+          <Button>{outcome === 'win' ? 'Play Again' : 'Try Again'}</Button>
+        </MessageContainer>
+      </MessageModal>
     )
     : <></> 
   )
